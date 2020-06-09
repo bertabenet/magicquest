aae6adcbec3e39ca3c973871cc6841d8f86d8a60
marilenabudan <elena.budan01@estudiant.upf.edu>
2020-06-09 13:56:44 +0200

    Commit

--- Assets/Scripts/Mountain/Handles.cs
+++ Assets/Scripts/Mountain/Handles.cs
@@ -9,20 +9,30 @@ public class Handles : MonoBehaviour
     public Material deactivateColor;
     public int level;
 
+    private bool isIn;
+    private string inObject;
+
+    private void Start()
+    {
+        isIn = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("BodyPart"))
+        if (other.CompareTag("BodyPart") && !isIn)
         {
+            isIn = true;
+            inObject = other.name;
             gameObject.GetComponent<Renderer>().material.color = activateColor.color;
             GameManagerMountain.Instance.IncreaseLevelCounter(level);
-
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("BodyPart"))
+        if (other.CompareTag("BodyPart") && isIn && inObject == other.name)
         {
+            isIn = false;
             gameObject.GetComponent<Renderer>().material.color = deactivateColor.color;
             GameManagerMountain.Instance.DecreaseLevelCounter(level);
         }
